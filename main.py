96f33dbde3043a8a3a3697197ab0d32434c4a09e
Edson Copque <ecop@disroot.org>
2025-06-13 15:02:14 -0300

    Random matrix creation.

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 # conways_game_of_life:
 
+import random
+
+WIDTH = 60 # column
+HEIGHT = 20 # lines
+
+# Random matrix creation:
+next_cells = []
+for i1 in WIDTH:
+    column = 0
+    for i2 in HEIGHT:
+        if (random.randint(0, 1) == 0):
+            column.append("#")
+        else:
+            column.append(" ")
+    next_cells.append(column)
+
+
+
 
