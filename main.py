3081c75e1990309a0b34cbf7791b8c137bf325cb
Edson Copque <ecop@disroot.org>
2025-06-14 10:34:41 -0300

    Adjustments on the code.

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 # conways_game_of_life:
 
-import random
+import random, time, copy
 
-WIDTH = 60 # column
-HEIGHT = 20 # lines
+WIDTH = 60 # column (x) / largura
+HEIGHT = 20 # lines (y) / altura
 
 # Random matrix creation:
 next_cells = []
-for i1 in WIDTH:
+for x in WIDTH:
     column = 0
-    for i2 in HEIGHT:
+    for y in HEIGHT:
         if (random.randint(0, 1) == 0):
             column.append("#")
         else:
             column.append(" ")
     next_cells.append(column)
 
+while True:
+    print("\n\n\n\n")
+    current_cells = copy.deepcopy(next_cells)
+
+    for y in range(HEIGHT):
+        for x in range(WIDTH):
+            print(current_cells[x][y], end="")
+        print()
 
 
 
