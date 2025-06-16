bcbcb46babd3b19df0e041f6023dc6b50e15f46b
Edson Copque <ecop@disroot.org>
2025-06-16 11:38:03 -0300

    feat: Positions of the neighbors.

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-# conways_game_of_life:
+# FILE: /main.py
+
+# CONWAYs GAME OF LIFE:
 
 import random, time, copy
 
 WIDTH = 60 # column (x) / largura
 HEIGHT = 20 # lines (y) / altura
 
-# Random matrix creation:
+# RANDOM MATRIX CREATION:
 next_cells = []
 for x in WIDTH:
     column = 0
@@ -16,7 +18,7 @@ for x in WIDTH:
             column.append(" ")
     next_cells.append(column)
 
-# Print the grid to the screen:
+# PRINT THE GRID TO THE SCREEN:
 while True:
     print("\n\n\n\n\n\n")
     current_cells = copy.deepcopy(next_cells)
@@ -26,6 +28,20 @@ while True:
             print(current_cells[x][y], end="")
         print()
 
+    # NEXT GENERATION CALCULATION:
+    for x in range(WIDTH):
+        for y in range(HEIGHT):
+        
+        # DEFINES THE POSITIONS OF THE NEIGHBORS:
+        # Take the neighbor on the left. If it is in the 1st column, go back to the last column.
+        left_coord = (x - 1) % WIDTH
+        # Take the neighbor on the right. If it is in the last column, it goes back to the first
+        # column.
+        right_coord = (x + 1) % WIDTH
+        # Neighbor above. If it is on the 1st line, it goes back do the last line.
+        above_coord = (y - 1) % HEIGHT
+        # Neighbot below. If it is on the last line, it goes back to the first line.
+        below_coord = (y + 1) % HEIGHT
 
 
 
