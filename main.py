ad1d5422fe6d80452dc804d5c88c86ef1dbd99f1
Edson Copque <ecop@disroot.org>
2025-06-21 11:28:05 -0300

    fixed: little bugs.

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ HEIGHT = 20 # lines (y) / altura
 
 # RANDOM MATRIX CREATION:
 next_cells = []
-for x in WIDTH:
-    column = 0
-    for y in HEIGHT:
+for x in range(WIDTH):
+    column = []
+    for y in range(HEIGHT):
         if (random.randint(0, 1) == 0):
             column.append("#")
         else:
@@ -32,41 +32,41 @@ while True:
     for x in range(WIDTH):
         for y in range(HEIGHT):
         
-        # DEFINES THE POSITIONS OF THE NEIGHBORS:
-        # Take the neighbor on the left. If it is in the 1st column, go back to the last column.
-        left_coord = (x - 1) % WIDTH
-        # Take the neighbor on the right. If it is in the last column, it goes back to the first
-        # column.
-        right_coord = (x + 1) % WIDTH
-        # Neighbor above. If it is on the 1st line, it goes back do the last line.
-        above_coord = (y - 1) % HEIGHT
-        # Neighbot below. If it is on the last line, it goes back to the first line.
-        below_coord = (y + 1) % HEIGHT
-        
-        num_neighbors = 0
-        if current_cells[left_coord][above_coord] == "#":
-            num_neighbors += 1
-        if current_cells[x][above_coord] == "#":
-            num_neighbors += 1
-        if current_cells[right_coord][above_coord] == "#":
-            num_neighbors += 1
-        if current_cells[left_coord][y] == "#":
-            num_neighbors += 1
-        if current_cells[right_coord][y] == "#":
-            num_neighbors += 1
-        if current_cells[left_coord][below_coord] == "#":
-            num_neighbors += 1
-        if current_cells[x][below_coord] == "#":
-            num_neighbors += 1
-        if current_cells[right_coord][below_coord] == "#":
-            num_neighbors += 1
- 
-        if current_cells[x][y] == "#" and (num_neighbors == 2 or num_neighbors == 3):
+            # DEFINES THE POSITIONS OF THE NEIGHBORS:
+            # Take the neighbor on the left. If it is in the 1st column, go back to the last column.
+            left_coord = (x - 1) % WIDTH
+            # Take the neighbor on the right. If it is in the last column, it goes back to the first
+            # column.
+            right_coord = (x + 1) % WIDTH
+            # Neighbor above. If it is on the 1st line, it goes back do the last line.
+            above_coord = (y - 1) % HEIGHT
+            # Neighbot below. If it is on the last line, it goes back to the first line.
+            below_coord = (y + 1) % HEIGHT
+            
+            num_neighbors = 0
+            if current_cells[left_coord][above_coord] == "#":
+                num_neighbors += 1
+            if current_cells[x][above_coord] == "#":
+                num_neighbors += 1
+            if current_cells[right_coord][above_coord] == "#":
+                num_neighbors += 1
+            if current_cells[left_coord][y] == "#":
+                num_neighbors += 1
+            if current_cells[right_coord][y] == "#":
+                num_neighbors += 1
+            if current_cells[left_coord][below_coord] == "#":
+                num_neighbors += 1
+            if current_cells[x][below_coord] == "#":
+                num_neighbors += 1
+            if current_cells[right_coord][below_coord] == "#":
+                num_neighbors += 1
+     
+            if current_cells[x][y] == "#" and (num_neighbors == 2 or num_neighbors == 3):
+                    next_cells[x][y] = "#"
+            elif current_cells[x][y] == " " and num_neighbors == 3:
                 next_cells[x][y] = "#"
-        elif current_cells[x][y] == " " and num_neighbors == 3:
-            next_cells[x][y] = "#"
-        else:
-            next_cells[x][y] = " "
+            else:
+                next_cells[x][y] = " "
     time.sleep(1)
 
             
